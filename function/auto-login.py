21e4abc9648b0917a3d1950066b6658c9207c721
light-ai <subarasiisekaiyuu.seraph@gmail.com>
2021-09-04 17:12:41 +0900

    Add auto-login

--- function/auto-login.py
+++ function/auto-login.py
@@ -2,29 +2,17 @@ import time
 from selenium import webdriver
 
 driver = webdriver.Chrome() # WebDriverのインスタンスを作成
-driver.get('https://ict-t.el.kyutech.ac.jp/') # URLを指定してブラウザを開く
+driver.get(r'file:///C:\Users\subar\Desktop\新しいフォルダー\auto-manegement\site\index.html') # URLを指定してブラウザを開く
 time.sleep(2) # 2秒待機
-driver.find_element_by_link_text("ログイン").click()
-search_box = driver.find_element_by_name('j_username') # name属性で検索ボックスを特定
-search_box1 = driver.find_element_by_name('j_password')
-search_box.send_keys('aeve2636') # 検索ボックスにテキストを入力
-search_box1.send_keys('hg9Xh6Vd')
+driver. find_element_by_xpath("//input[@id = 'username']").send_keys('三木彪瑠')
+time.sleep(1)
+driver. find_element_by_xpath("//input[@id = 'password']").send_keys('mikitakeru')
+time.sleep(1)
 driver. find_element_by_xpath("//button[text() = 'Login']").click()
-time.sleep(2) # 2秒待機
 #driver.find_element_by_link_text("システム工学").click()
 #driver.find_elements_by_tag_id("yui_3_17_2_1_1630728402296_179").click()
+time.sleep(2) # 2秒待機
+driver.find_element_by_link_text("電磁気学Ⅰ(2021):共通:Q01:月曜日4時限木曜日2時限(1)").click()
 time.sleep(2)
-driver.find_element_by_xpath("//div[@data-region='drawer-toggle']").click()
-time.sleep(2)
-driver.find_element_by_xpath("//a[@data-key='home']").click()
-time.sleep(2)
-driver.find_element_by_xpath("//div[@data-region='drawer-toggle']").click()
-driver.find_element_by_xpath("//input[@name='q']").send_keys("宇宙システム利用(2021)")
-time.sleep(2)
-driver. find_element_by_xpath("//button[text() = 'Go']").click()
-time.sleep(2)
-driver. find_element_by_xpath("//a[@class = 'aalink']").click()
-time.sleep(2)
-#driver. find_element_by_xpath("//a[@class = 'aalink']").click()
-driver. find_element_by_xpath("//span[text() = '出欠']").click()
-#driver.quit() # ブラウザを閉じる
+driver. find_element_by_xpath("//button[text() = '出席']").click()
+driver.quit() # ブラウザを閉じる
